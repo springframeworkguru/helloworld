80112d4669a7a851cd5543303bc7c038a3d8034c
John Thompson <john@springframework.guru>
2018-12-07 20:52:07 -0500

    adding auto clean

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,15 @@
+/**
+ * Created by jt on 2018-12-07.
+ */
+public class HelloWorldTest {
+
+    public void testHello(){
+
+        System.out.println("TEST - Hello...");
+    }
+
+    public void testWorld() {
+        System.out.println("TEST - World!!!!");
+    }
+
+}
