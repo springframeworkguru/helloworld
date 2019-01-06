afcc279fb61a39bbf0812775fb7e8cb52a36ff92
ggab78 <gabriel.gamrat@gmail.com>
2019-01-06 17:09:55 +0100

    commit HelloWorld.java src

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,14 @@
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Created by jt on 2018-11-26.
  */
 public class HelloWorld {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!!!! ");
+        System.out.println(StringUtils.capitalize("hello world"));
+
     }
 }
