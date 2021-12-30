0b448baa605f4c60c565e2f47abae019fbce1626
Oleksii Riabov <ryaboff99@gmail.com>
2021-12-30 12:08:28 +0200

    Update HelloWorld.java
    * add capitalized "hello world" output
    * add StringUtils import
    * add StringUtils dependency

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,3 +1,4 @@
+import org.apache.commons.lang3.StringUtils;
 /**
  * Created by jt on 2018-11-26.
  */
@@ -5,5 +6,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!!!! ");
+        System.out.println(StringUtils.capitalize("hello world"));
     }
 }
